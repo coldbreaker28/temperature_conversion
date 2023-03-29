9866683ca6db53a15d9931bb027048f508d9ed6d
coldbreaker28 <anzanononk@gmail.com>
2023-03-29 12:38:28 +0700

    Renew t_c

--- lib/class/MyHome.dart
+++ lib/class/MyHome.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:temperature_conversion/class/convert.dart';
 import 'package:temperature_conversion/class/input.dart';
+import 'package:temperature_conversion/class/Result.dart';
 
 class MyHome extends StatefulWidget {
   const MyHome({Key? key, required this.title}) : super(key: key);
@@ -13,12 +14,20 @@ class MyHome extends StatefulWidget {
 
 class _MyHomeState extends State<MyHome> {
   double _inputUser = 0;
-  double _Kelvin = 0;
+  double _kelvin = 0;
   double _reamur = 0;
   double _fahrenheit = 0;
   double _rankine = 0;
+  String _newValue = "Kelvin";
+  double _result = 0;
+
+  var listItem = ["Kelvin", "Reamur", "Fahrenheit", "Rankine"];
 
   final myController = TextEditingController();
+
+  get Key => null;
+
+  List<String> listViewItem = <String>[];
 
   @override
   void dispose() {
@@ -29,11 +38,19 @@ class _MyHomeState extends State<MyHome> {
   void konversiSuhu() {
     setState(() {
       _inputUser = double.parse(myController.text);
-      _reamur = (4 / 5) * _inputUser;
-      _Kelvin = 273.15 + _inputUser;
-      _fahrenheit = (9 / 5) * _inputUser + 32;
-      _rankine = (_inputUser + 273.15) * 9 / 5;
+
+      if (_newValue == "Kelvin") {
+        _result = 273.15 + _inputUser;
+      } else if (_newValue == "Fahrenheit") {
+        _result = (9 / 5) * _inputUser + 32;
+      } else if (_newValue == "Rankine") {
+        _result = (_inputUser + 273.15) * 9 / 5;
+      } else {
+        _result = (4 / 5) * _inputUser;
+      }
     });
+    listViewItem.add(_newValue.toString());
+    listViewItem.add(_result.toString());
   }
 
   @override
@@ -45,101 +62,47 @@ class _MyHomeState extends State<MyHome> {
       body: Container(
         margin: const EdgeInsets.all(8),
         child: ListView(
-          children: <Widget>[
+          children: [
             Input(myController: myController),
-            Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      // ignore: prefer_const_constructors
-                      Text(
-                        "Suhu Dalam Celcius",
-                        style: const TextStyle(fontSize: 14),
-                        textAlign: TextAlign.center,
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Colors.blue,
-                          width: 5,
-                        )),
-                        child: Text(_inputUser.toStringAsFixed(2),
-                            style: const TextStyle(fontSize: 20)),
-                      ),
-                      // ignore: prefer_const_constructors
-                      Text(
-                        "Suhu Dalam Kelvin",
-                        style: const TextStyle(fontSize: 14),
-                        textAlign: TextAlign.center,
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Colors.blue,
-                          width: 5,
-                        )),
-                        child: Text(_Kelvin.toStringAsFixed(2),
-                            style: const TextStyle(fontSize: 20)),
-                      ),
-                      // ignore: prefer_const_constructors
-                      Text(
-                        "Suhu Dalam fahrenheit",
-                        style: const TextStyle(fontSize: 14),
-                        textAlign: TextAlign.center,
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Colors.blue,
-                          width: 5,
-                        )),
-                        child: Text(_fahrenheit.toStringAsFixed(2),
-                            style: const TextStyle(fontSize: 20)),
-                      ),
-                      // ignore: prefer_const_constructors
-                      Text(
-                        "Suhu Dalam reamur",
-                        style: const TextStyle(fontSize: 14),
-                        textAlign: TextAlign.center,
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Colors.blue,
-                          width: 5,
-                        )),
-                        child: Text(_reamur.toStringAsFixed(2),
-                            style: const TextStyle(fontSize: 20)),
-                        // ignore: prefer_const_constructors
-                      ),
-                      // ignore: prefer_const_constructors
-                      Text(
-                        "Suhu Dalam rankine",
-                        style: const TextStyle(fontSize: 14),
-                        textAlign: TextAlign.center,
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Colors.blue,
-                          width: 5,
-                        )),
-                        child: Text(_rankine.toStringAsFixed(2),
-                            style: const TextStyle(fontSize: 20)),
-                      )
-                    ],
-                  )
-                ]),
+            DropdownButton<String>(
+                items: listItem.map(myDropDownMenuItem).toList(),
+                value: _newValue,
+                onChanged: onChangeDropDown),
+            Result(result: _result),
             Convert(konvertHandler: konversiSuhu),
+            Container(
+              margin: const EdgeInsets.only(top: 10, bottom: 10),
+              child: const Text(
+                "Riwayat Konversi",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Expanded(
+                child: Column(
+              children: listViewItem.map((String value) {
+                return Container(
+                  margin: const EdgeInsets.all(10),
+                  child: Text(
+                    value,
+                    style: const TextStyle(fontSize: 15),
+                  ),
+                );
+              }).toList(),
+            )),
           ],
         ),
       ),
     );
+  }
+
+  void onChangeDropDown(String? changeValue) {
+    setState(() {
+      _newValue = changeValue!;
+      konversiSuhu();
+    });
+  }
+
+  DropdownMenuItem<String> myDropDownMenuItem(String value) {
+    return DropdownMenuItem<String>(value: value, child: Text(value));
   }
 }
